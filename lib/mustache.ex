fce9624e0d052b8b46500a3b377a76d3f9b4c99b
Megan McClure <mmcclure0100@gmail.com>
2021-08-05 16:35:08 -0500

    Allow string keys in dot patterns

--- lib/mustache.ex
+++ lib/mustache.ex
@@ -1,9 +1,9 @@
 defmodule Mustache do
-
-  def render(template, data \\%{}) do
-    Enum.reduce(strategies(), template, fn(strategy, template) ->
+  def render(template, data \\ %{}) do
+    Enum.reduce(strategies(), template, fn strategy, template ->
       predicate = elem(strategy, 0)
       function = elem(strategy, 1)
+
       if predicate.(template) do
         function.(template, data)
       else
@@ -13,19 +13,25 @@ defmodule Mustache do
   end
 
   defp double_mustaches(template, data) do
-    scans = Regex.scan(double_regex(), template) |> List.flatten
+    scans = Regex.scan(double_regex(), template) |> List.flatten()
+
     case scans do
-      [] -> template
-      _  ->
+      [] ->
+        template
+
+      _ ->
         first_scan = List.first(scans)
         variable = first_scan |> clean(["{{", "}}"])
-        value = if escape?(first_scan) do
-          key = variable |> String.strip
-          data |> indifferent_access(key) |> to_string |> escape
-        else
-          key = String.replace(variable, "&", "") |> String.strip
-          data |> indifferent_access(key) |> to_string
-        end
+
+        value =
+          if escape?(first_scan) do
+            key = variable |> String.strip()
+            data |> indifferent_access(key) |> to_string |> escape
+          else
+            key = String.replace(variable, "&", "") |> String.strip()
+            data |> indifferent_access(key) |> to_string
+          end
+
         if value == nil do
           template
         else
@@ -34,29 +40,97 @@ defmodule Mustache do
     end
   end
 
+  defp process_section(template, data) do
+    matches = Regex.run(section_regex(), template)
+
+    case matches do
+      nil ->
+        template
+
+      [full, predicate, var, body] ->
+        val = indifferent_access(data, var)
+
+        section_val =
+          case predicate do
+            "#" -> process_if(body, val)
+            "^" -> process_unless(body, val)
+          end
+
+        process_section(String.replace(template, full, section_val), data)
+    end
+  end
+
+  defp process_if(template, val) do
+    case val do
+      nil ->
+        ""
+
+      false ->
+        ""
+
+      [] ->
+        ""
+
+      [_ | _] ->
+        val
+        |> Stream.map(&render(template, &1))
+        |> Enum.join()
+
+      val ->
+        render(template, val)
+    end
+  end
+
+  defp process_unless(template, val) do
+    case val do
+      nil -> render(template, val)
+      false -> render(template, val)
+      [] -> render(template, val)
+      _val -> ""
+    end
+  end
+
   defp indifferent_access(map, string_key) do
-    map[string_key] || map[string_key |> String.to_atom]
+    case Access.get(map, string_key) do
+      nil -> Access.get(map, resolve_key(string_key))
+      val -> val
+    end
+  end
+
+  defp resolve_key(key) do
+    try do
+      String.to_existing_atom(key)
+    rescue
+      ArgumentError -> key
+    end
   end
 
   defp scan_for_dot(template, data) do
     regex = regex("{{", "}}", "\\w+(\\.\\w+)+")
     matches = Regex.run(regex, template)
+
     case matches do
-      nil -> template
-      _  ->
+      nil ->
+        template
+
+      _ ->
         path = List.first(matches) |> clean(["{{", "}}"])
         scan_for_dot(interpolate(template, data, path), data)
     end
   end
 
   defp triple_mustaches(template, data) do
-    scans = Regex.scan(triple_regex(), template) |> List.flatten
+    scans = Regex.scan(triple_regex(), template) |> List.flatten()
+
     case scans do
-      [] -> template
-      _  ->
+      [] ->
+        template
+
+      _ ->
         variable = List.first(scans) |> clean(["{{{", "}}}"])
-        key = variable |> String.strip
+        key = variable |> String.strip()
         value = data |> indifferent_access(key) |> to_string
+
         if value == nil do
           template
         else
@@ -70,12 +144,12 @@ defmodule Mustache do
     String.replace(template, "{{#{path}}}", to_string(value))
   end
 
-  defp resolve(data, path) do
-    key = String.to_atom(hd(path))
-    case tl(path) do
-      [] -> data[key]
-      _  -> resolve(data[key], tl(path))
-    end
+  def resolve(data, [key | []]), do: indifferent_access(data, key)
+
+  def resolve(data, [key | rest]) do
+    data
+    |> indifferent_access(key)
+    |> resolve(rest)
   end
 
   defp double_regex do
@@ -90,29 +164,38 @@ defmodule Mustache do
     Regex.compile!("#{otag}#{body}#{ctag}")
   end
 
+  defp section_regex do
+    ~r<{{\s*(#|\^)\s*([\w.]+)\s*}}(.*?){{\s*/\s*\2\s*}}>
+  end
+
   defp escape?(template) do
     !String.contains?(template, "&")
   end
 
   defp escape(non_escaped) do
-    forbidden = [{"&", "&amp;"}, {"<","&lt;" }, {">", "&gt;"}, {"\"", "&quot;"}]
-    Enum.reduce(forbidden, non_escaped, fn (x, str) ->
+    forbidden = [{"&", "&amp;"}, {"<", "&lt;"}, {">", "&gt;"}, {"\"", "&quot;"}]
+
+    Enum.reduce(forbidden, non_escaped, fn x, str ->
       String.replace(str, elem(x, 0), elem(x, 1))
     end)
   end
 
   defp clean(non_cleaned, patterns) do
-    Enum.reduce(patterns, non_cleaned, fn(pattern, str) ->
+    Enum.reduce(patterns, non_cleaned, fn pattern, str ->
       String.replace(str, pattern, "")
     end)
   end
 
   defp strategies do
-    [{ fn(template) -> Regex.match?(triple_regex(), template) end,
-        fn(template, data) -> triple_mustaches(template, data) end},
-    { fn(template) -> Regex.match?(regex("{{", "}}", "\\w+(\\.\\w+)+"), template) end,
-        fn(template, data) -> scan_for_dot(template, data) end },
-    { fn(template) -> Regex.match?(double_regex(), template) end,
-        fn(template, data) -> double_mustaches(template, data) end}]
+    [
+      {fn template -> Regex.match?(section_regex(), template) end,
+       fn template, data -> process_section(template, data) end},
+      {fn template -> Regex.match?(triple_regex(), template) end,
+       fn template, data -> triple_mustaches(template, data) end},
+      {fn template -> Regex.match?(regex("{{", "}}", "\\w+(\\.\\w+)+"), template) end,
+       fn template, data -> scan_for_dot(template, data) end},
+      {fn template -> Regex.match?(double_regex(), template) end,
+       fn template, data -> double_mustaches(template, data) end}
+    ]
   end
 end
--- test/mustache_feature_test.exs
+++ test/mustache_feature_test.exs
@@ -2,7 +2,7 @@ defmodule MustacheFeatureTest do
   use ExUnit.Case
 
   test "No Interpolation" do
-    assert Mustache.render("Hello from {Mustache}!\n")  == "Hello from {Mustache}!\n"
+    assert Mustache.render("Hello from {Mustache}!\n") == "Hello from {Mustache}!\n"
   end
 
   test "Basic Interpolation" do
@@ -10,15 +10,21 @@ defmodule MustacheFeatureTest do
   end
 
   test "HTML Escaping" do
-    assert Mustache.render("These characters should be HTML escaped: {{forbidden}}\n", %{forbidden: "& \" < >"}) == "These characters should be HTML escaped: &amp; &quot; &lt; &gt;\n"
+    assert Mustache.render("These characters should be HTML escaped: {{forbidden}}\n", %{
+             forbidden: "& \" < >"
+           }) == "These characters should be HTML escaped: &amp; &quot; &lt; &gt;\n"
   end
 
   test "Triple Mustache" do
-    assert Mustache.render("These characters should not be HTML escaped: {{{forbidden}}}\n", %{forbidden: "& \" < >"}) == "These characters should not be HTML escaped: & \" < >\n"
+    assert Mustache.render("These characters should not be HTML escaped: {{{forbidden}}}\n", %{
+             forbidden: "& \" < >"
+           }) == "These characters should not be HTML escaped: & \" < >\n"
   end
 
   test "Ampersand" do
-    assert Mustache.render("These characters should not be HTML escaped: {{&forbidden}}\n", %{forbidden: "& \" < >"}) == "These characters should not be HTML escaped: & \" < >\n"
+    assert Mustache.render("These characters should not be HTML escaped: {{&forbidden}}\n", %{
+             forbidden: "& \" < >"
+           }) == "These characters should not be HTML escaped: & \" < >\n"
   end
 
   test "Integers should interpolate seamlessly." do
@@ -38,7 +44,8 @@ defmodule MustacheFeatureTest do
   end
 
   test "Triple Mustache Decimal Interpolation" do
-    assert Mustache.render("\"{{{power}}} jiggawatts!\"", %{power: 1.21}) == "\"1.21 jiggawatts!\""
+    assert Mustache.render("\"{{{power}}} jiggawatts!\"", %{power: 1.21}) ==
+             "\"1.21 jiggawatts!\""
   end
 
   test "Ampersand Decimal Interpolation" do
@@ -57,59 +64,108 @@ defmodule MustacheFeatureTest do
     assert Mustache.render("I ({{&cannot}}) be seen!", %{}) == "I () be seen!"
   end
 
-  #Dotted Names
+  # Dotted Names
 
   test "Dotted Names" do
-    assert Mustache.render("\"{{person.name}}\" == \"Joe\"",
-             %{person: %{name: "Joe"}}) == "\"Joe\" == \"Joe\""
-    assert Mustache.render("\"{{person.name.first}}\" == \"Joe\"",
-             %{person: %{name: %{first: "Joe"}}}) == "\"Joe\" == \"Joe\""
+    assert Mustache.render(
+             "\"{{person.name}}\" == \"Joe\"",
+             %{person: %{name: "Joe"}}
+           ) == "\"Joe\" == \"Joe\""
+
+    assert Mustache.render(
+             "\"{{person.name.first}}\" == \"Joe\"",
+             %{person: %{name: %{first: "Joe"}}}
+           ) == "\"Joe\" == \"Joe\""
+  end
+
+  test "Dotted Names - String Keys" do
+    assert Mustache.render(
+             "\"{{person.name}}\" == \"Joe\"",
+             %{"person" => %{"name" => "Joe"}}
+           ) == "\"Joe\" == \"Joe\""
   end
 
   @tag :pending
-  test "Dotted Names - Basic Interpolation" do
-    assert Mustache.render("\"{{person.name}}\" == \"{{#person}}{{name}}{{/person}}\"",
-              %{person: %{name: "Joe"}}) == "\"Joe\" == \"Joe\""
+  test "Dotted Names - Mixed Keys" do
+    assert Mustache.render(
+             "\"{{person.name}}\" == \"Joe\"",
+             %{"person" => %{name: "Joe"}}
+           ) == "\"Joe\" == \"Joe\""
   end
 
-  #Whitespace sensitivity
+  # Sections
+
+  test "Section Interpolation" do
+    assert Mustache.render(
+             "\"{{person.name}}\" == \"{{#person}}{{name}}{{/person}}\"",
+             %{person: %{name: "Joe"}}
+           ) == "\"Joe\" == \"Joe\""
+  end
+
+  test "Section Interpolation - Inverse" do
+    assert Mustache.render(
+             "\"{{person.name}}\" == \"{{^person}}{{name}}{{/person}}\"",
+             %{person: %{name: "Joe"}}
+           ) == "\"Joe\" == \"\""
+  end
+
+  test "Section Interpolation - Basic and Inverse" do
+    assert Mustache.render(
+             "\"{{person.name}}\" == \"{{#person_2}}{{name}}{{/person_2}}{{^person_2}}Joe{{/person_2}}\"",
+             %{person: %{name: "Joe"}}
+           ) == "\"Joe\" == \"Joe\""
+  end
+
+  test "Section Interpolation - list values" do
+    assert Mustache.render(
+             "\"{{#people}}{{name}} {{/people}}\"",
+             %{people: [%{name: "Joe"}, %{name: "Jill"}]}
+           ) == "\"Joe Jill \""
+  end
+
+  test "render/2 returns rendered template when key is string" do
+    assert Mustache.render("Hello {{custom.candidate}}.", %{
+             custom: %{"candidate" => "Palpatine"}
+           }) == "Hello Palpatine."
+  end
+
+  # Whitespace sensitivity
 
   test "Interpolation - Surrounding Whitespace" do
     assert Mustache.render("| {{string}} |", %{string: '---'}) == "| --- |"
   end
 
-
   test "Triple Mustache - Surrounding Whitespace" do
-    assert Mustache.render("| {{{string}}} |", %{ string: '---' }) == "| --- |"
+    assert Mustache.render("| {{{string}}} |", %{string: '---'}) == "| --- |"
   end
 
   test "Ampersand - Surrounding Whitespace" do
-    assert Mustache.render("| {{&string}} |", %{string: '---' }) == "| --- |"
+    assert Mustache.render("| {{&string}} |", %{string: '---'}) == "| --- |"
   end
 
   test "Interpolation - Standalone" do
-    assert Mustache.render("  {{string}}\n", %{string: '---' }) == "  ---\n"
+    assert Mustache.render("  {{string}}\n", %{string: '---'}) == "  ---\n"
   end
 
   test "Triple Mustache - Standalone" do
-    assert Mustache.render("  {{{string}}}\n", %{ string: '---' }) == "  ---\n"
+    assert Mustache.render("  {{{string}}}\n", %{string: '---'}) == "  ---\n"
   end
 
   test "Ampersand - Standalone" do
-    assert Mustache.render("  {{&string}}\n", %{ string: '---' }) == "  ---\n"
+    assert Mustache.render("  {{&string}}\n", %{string: '---'}) == "  ---\n"
   end
 
-   # Whitespace Insensitivity
+  # Whitespace Insensitivity
 
-   test "Interpolation With Padding" do
-     assert Mustache.render("|{{ string }}|", %{ string: "---" }) == "|---|"
-   end
+  test "Interpolation With Padding" do
+    assert Mustache.render("|{{ string }}|", %{string: "---"}) == "|---|"
+  end
 
-   test "Triple Mustache With Padding" do
-     assert Mustache.render("|{{{ string }}}|", %{ string: "---" }) == "|---|"
-   end
+  test "Triple Mustache With Padding" do
+    assert Mustache.render("|{{{ string }}}|", %{string: "---"}) == "|---|"
+  end
 
-   test "Ampersand With Padding" do
-     assert Mustache.render("|{{& string }}|", %{ string: "---" }) == "|---|"
-   end
+  test "Ampersand With Padding" do
+    assert Mustache.render("|{{& string }}|", %{string: "---"}) == "|---|"
+  end
 end
